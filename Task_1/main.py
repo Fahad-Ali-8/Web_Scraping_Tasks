ed0b58f01f77dcfed8b6c7de1b973fd01faeb898
Fahad Ali <fahadali45621@gmail.com>
2026-04-05 12:55:01 +0500

    task 1 complete 80%

--- Task_1/main.py
+++ Task_1/main.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import time
+import pandas as pd
 
 url = "https://remoteok.com/"
 
@@ -23,10 +24,10 @@ driver.get(url)
 time.sleep(5)
 
 # scrolling to load more jobs
-#print("Scrolling to load more jobs")
-#for i in range(5):
-#driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-#time.sleep(2)
+print("Scrolling to load more jobs")
+for i in range(5):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+    time.sleep(2)
 
 # parsing with beautifulsoup
 print("parsing jobs with beautifulsoup")
@@ -34,24 +35,36 @@ soup = BeautifulSoup(driver.page_source,"lxml")
 driver.quit()
 
 # # finding all job titles
+jobs = []
 job_rows = soup.select("tr.job")
 for job in job_rows:
     title = job.select_one("h2[itemprop='title']")
     name = job.select_one("span.companyLink")
     location = job.select_one("div.location")
     job_link = job.get("data-href")
+    tags = [tag.text.strip() for tag in job.select("span.tag")]
 
     title = title.text.strip() if title else "N/A"
     name = name.text.strip() if name else "N/A"
     location = location.text.strip() if location else "Remote"
-    Job_link = "https://remoteok.com/" +job_link if job_link else "N/A"
+    job_link = "https://remoteok.com/" +job_link if job_link else "N/A"
+    tags = tags if tags else "N/A"
 
-    print(f"job_title        :{title}")    
-    print(f"compnay name     :{name}")
-    print(f"compnay location :{location}")
-    print(f"job link         :{Job_link}")
-    print("")
+    if "Software" in title:
+        jobs.append({
+        "job_title"     :title,   
+        "compnay name"     :name,
+        "compnay location" :location,
+        "job link"         :job_link,
+        "tags"             :tags
+        })
 
-# # finding company names
-# compnay_rows = soup.select("td.compnay")
-# for compnay in compnay_rows:
+# ─── STEP 5: SAVE TO CSV ─────────────────────────────
+if jobs:
+    df = pd.DataFrame(jobs)
+    df.to_csv("Task_1/jobs.csv", index=False, encoding="utf-8")
+    print(f"\n Scraped {len(jobs)} jobs successfully!")
+    print("Saved to jobs.csv")
+    # print(df.head())
+else:
+    print("No jobs found!")

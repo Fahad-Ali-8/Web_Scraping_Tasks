1e2ea322fc629caa7e71e141c74daeb4ee6fec12
Fahad Ali <fahadali45621@gmail.com>
2026-04-10 11:46:19 +0500

    initial commit

---
+++ CoinMarketCap_Scraper/main.py
@@ -0,0 +1,91 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import time
+import os
+import csv
+from urllib.parse import urljoin
+
+# config
+BASE_URL = "https://coinmarketcap.com/"
+OUTPUT_FILE = "crypto.csv"
+HEADERS = ["name", "icon", "price"]
+
+# setup
+def init_driver():
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+    return driver
+
+# WRITE CSV HEADERS UPFRONT (chunk-saving)
+def init_csv(filepath):
+    directory = os.path.dirname(filepath)
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+    with open(filepath, "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=HEADERS)
+        writer.writeheader()
+    print(f"CSV initialized: {filepath}")
+
+# APPEND ONE PRODUCT TO CSV 
+def save_product(filepath, product):
+    with open(filepath, "a", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=HEADERS)
+        writer.writerow(product)
+
+def scroll_page(driver):    
+    for _ in range(20):
+        driver.execute_script("window.scrollBy(0, 500)")
+        time.sleep(0.5)
+    
+    time.sleep(3)  
+    print("Scrolling done.")
+
+# scraping data
+def scrape_data(driver,filepath):
+    wait = WebDriverWait(driver , 10)
+    driver.get(BASE_URL)
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "tbody tr")))
+    # scroll to load all rows
+    scroll_page(driver)
+
+    soup = BeautifulSoup(driver.page_source,"lxml")
+
+    # targeting data
+    rows = soup.select("tbody tr")
+    for row in rows:
+        name = row.select_one("td:nth-child(3) p")
+        icon = row.select_one(".coin-item-symbol")
+        price = row.select_one(".span")
+        crypto_data = {
+        "name"  : name.get_text(strip=True) if name else "N/A",
+        "icon"  : icon.get_text(strip=True) if icon else "N/A",
+        "price"  : price.get_text(strip=True) if price else "N/A"
+        }
+        save_product(filepath, crypto_data) 
+
+# main
+def main():
+    driver = init_driver()
+    init_csv(OUTPUT_FILE)
+
+    try:
+            scrape_data(driver,OUTPUT_FILE)
+            
+            # print(f"[{i}/{len(urls)}] Saved: {product['name']}")
+
+    finally:
+        time.sleep(5)
+        driver.quit()
+        print("Done! All products saved to products.csv")
+
+
+if __name__ == "__main__":
+    main()
+
+

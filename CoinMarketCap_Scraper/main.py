1c449825d8350a1b3f672be84b154998ea61fa98
Fahad Ali <fahadali45621@gmail.com>
2026-04-10 12:52:52 +0500

    added pagination

--- CoinMarketCap_Scraper/main.py
+++ CoinMarketCap_Scraper/main.py
@@ -12,9 +12,10 @@ import csv
 from urllib.parse import urljoin
 
 # config
-BASE_URL = "https://coinmarketcap.com/"
+BASE_URL = "https://coinmarketcap.com/?page={page_num}"
 OUTPUT_FILE = "crypto.csv"
-HEADERS = ["name", "icon", "price"]
+HEADERS = ["name", "icon", "price","24h'%'change", "marketcap"]
+TOTAL_PAGES = 5
 
 # setup
 def init_driver():
@@ -47,9 +48,10 @@ def scroll_page(driver):
     print("Scrolling done.")
 
 # scraping data
-def scrape_data(driver,filepath):
+def scrape_data(driver,page_num,filepath):
+    url = BASE_URL.format(page_num=page_num)
+    driver.get(url)
     wait = WebDriverWait(driver , 10)
-    driver.get(BASE_URL)
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "tbody tr")))
     # scroll to load all rows
     scroll_page(driver)
@@ -61,11 +63,16 @@ def scrape_data(driver,filepath):
     for row in rows:
         name = row.select_one("td:nth-child(3) p")
         icon = row.select_one(".coin-item-symbol")
-        price = row.select_one(".span")
+        price = row.select_one("td:nth-child(4) span")
+        _24h_change = row.select_one("td:nth-child(5) span")
+        marketcap = row.select_one("td:nth-child(8) span")
+
         crypto_data = {
         "name"  : name.get_text(strip=True) if name else "N/A",
         "icon"  : icon.get_text(strip=True) if icon else "N/A",
-        "price"  : price.get_text(strip=True) if price else "N/A"
+        "price"  : price.get_text(strip=True) if price else "N/A",
+        "24h'%'change" : _24h_change.get_text(strip=True) if _24h_change else "N/A",
+        "marketcap" : marketcap.get_text(strip=True) if marketcap else "N/A",
         }
         save_product(filepath, crypto_data) 
 
@@ -75,14 +82,15 @@ def main():
     init_csv(OUTPUT_FILE)
 
     try:
-            scrape_data(driver,OUTPUT_FILE)
+        for page_num in range(1, TOTAL_PAGES + 1):
+            scrape_data(driver,page_num,OUTPUT_FILE)
             
             # print(f"[{i}/{len(urls)}] Saved: {product['name']}")
 
     finally:
         time.sleep(5)
         driver.quit()
-        print("Done! All products saved to products.csv")
+        print("Done! All data id saved to {OUTPUT_FILE}")
 
 
 if __name__ == "__main__":
